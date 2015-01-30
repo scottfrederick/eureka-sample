a321f1976c36cf5398b9d676ea01a7463e091e72
Scott Frederick <scottyfred@gmail.com>
2015-01-30 10:13:24 -0600

    Initial commit.

---
+++ src/main/java/org/springframework/cloud/netflix/eureka/sample/Application.java
@@ -0,0 +1,15 @@
+package org.springframework.cloud.netflix.eureka.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
