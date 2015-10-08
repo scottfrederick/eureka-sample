f71f767c8391e50722d79cef71bea6c4be16c051
Scott Frederick <scottyfred@gmail.com>
2015-10-08 13:51:57 -0500

    Update to latest spring-cloud-services dependencies.

--- src/main/java/org/springframework/cloud/netflix/eureka/sample/config/CloudConfig.java
+++ src/main/java/org/springframework/cloud/netflix/eureka/sample/config/CloudConfig.java
@@ -1,16 +1,16 @@
 package org.springframework.cloud.netflix.eureka.sample.config;
 
-import com.netflix.discovery.EurekaClientConfig;
 import io.pivotal.spring.cloud.config.java.CloudConnectorsConfig;
+import org.springframework.cloud.netflix.eureka.EurekaClientConfigBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-//@Configuration
-//@Profile("cloud")
+@Configuration
+@Profile("cloud")
 public class CloudConfig extends CloudConnectorsConfig {
 	@Bean
-	public EurekaClientConfig eurekaClientConfig() {
+	public EurekaClientConfigBean eurekaClientConfig() {
 		return connectionFactory().eurekaClientConfig();
 	}
 }
